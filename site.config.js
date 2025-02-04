9626597526afa9dd7071f34d17db37b56313a38d
caputdraconis <caputdraconis@kakao.com>
2025-02-04 21:58:00 +0900

    update project's title & links

--- site.config.js
+++ site.config.js
@@ -16,8 +16,8 @@ const CONFIG = {
       href: "https://www.youtube.com/watch?v=RRWL4VfMY4k&list=PLX2fs3661XpPCc9wfHkc16TSs4DsNWL6R&index=14",
     },
     {
-      name: `geul-go`,
-      href: "https://github.com/caputdraconis050630/geul-go",
+      name: `geulk-go`,
+      href: "https://github.com/caputdraconis050630/geulk-go",
     },
   ],
   // blog setting (required)
